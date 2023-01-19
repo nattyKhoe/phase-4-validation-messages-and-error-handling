473cabaf91a62912cb55f48be64bf97e146ad6c0
Natasha <natasha.khoe@gmail.com>
2023-01-19 14:35:25 +0800

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   wrap_parameters false
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   
   def index
     movies = Movie.all
@@ -7,7 +8,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
   end
 
@@ -17,4 +18,9 @@ class MoviesController < ApplicationController
     params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
   end
   
+  def render_unprocessable_entity(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+  
+
 end
